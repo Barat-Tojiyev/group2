fa3cb166a6d964d09515c3458dd3314094919638
Barat-Tojiyev <barattojiyev@gmail.com>
2022-09-15 11:08:54 +0500

    uyga vazifa

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React ,{Component} from "react";
+import Card from "./Component/Card";
+class App extends Component{
+    render(){
+        return(
+            <div className=" bg-blue-500 p-9 ">
+                <Card/>
+            </div>
+        )
+    }
+}
+export default App
---
+++ src/Component/Card.js
@@ -0,0 +1,12 @@
+import React ,{ Component } from "react";
+
+
+class Card extends Component{
+    render(){
+        return(
+            <h1 className=" bg-white  text-7xl  p-8 text-blue-600">Card</h1>
+        )
+    }
+}
+
+export default Card
