a6637b091c557e18f93a381b6a9090989809e112
Barat-Tojiyev <barattojiyev@gmail.com>
2022-09-17 09:41:03 +0500

    sass

--- src/Component/Card.js
+++ src/Component/Card.js
@@ -4,12 +4,20 @@ import React ,{ Component } from "react";
 class Card extends Component{
     render(){
         return(
-            <div className=" bg-white   w-[600px] rounded-lg  p-8 text-blue-600">
-              <h1 className="text-4xl">Sign Up</h1>
-              <div>
-                <input className="px-3 py-3"/>
+            <div className=" bg-white   w-[800px] rounded-lg  p-8">
+              <h1 className="text-6xl pb-9 mx-auto w-[90%] ">Sign Up</h1>
+              <div className="mt-9 mx-auto w-[90%]">
+                <h1 className="text-4xl">Name:</h1>
+                <input type='text' className="px-3 py-3 outline-0 text-2xl border-[#999]  rounded-lg border-4 w-[100%]"/>
               </div>
-              
+              <div className="mt-9 mx-auto w-[90%]">
+                <h1 className="text-4xl">Password:</h1>
+                <input type='password' className="px-3 py-3 outline-0 text-2xl border-[#999] rounded-lg border-4 w-[100%]"/>
+              </div>
+              <div className="mt-9 mx-auto w-[90%]">
+                <button className="bg-[#EE5784] w-[100%] text-[30px] px-3 py-3 text-white rounded-xl font-semibold">Sign Up</button>
+              </div>
+              <hr className="mt-5 mx-auto w-[90%] border-y-2 border-[#999] bg-[#999] "/>
                 </div>
         )
     }
---
+++ src/Component/Card2.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Card2 = () => {
+  return (
+    <div>Card2</div>
+  )
+}
